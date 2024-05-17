fb37ba3395adbf5fd55b0d8f38860bf38684bc88
jainab11 <shaikhjainab11@gmail.com>
2024-05-17 15:40:54 +0530

    [Jainab] Add: python script for writing set

---
+++ set/01_.py
@@ -0,0 +1,21 @@
+'''
+
+@Author: sheikh jainab
+
+@Date: 2024-05-17 
+
+@Last Modified by: sheikh jainab
+
+@Last Modified time: 2024-05-17
+
+@Title : 1. Write a Python program to create a set.
+
+'''
+
+def main():
+    my_set = {1,2,3,4,5}
+    print(my_set)
+    print(type(my_set))
+    
+if __name__ == "__main__":
+    main()
