5badf4e39c9a679c0bfd820fa4c4803777de31bb
jainab11 <shaikhjainab11@gmail.com>
2024-05-17 15:43:14 +0530

    [jainab] Add: python script to itrate over array

---
+++ set/02_.py
@@ -0,0 +1,21 @@
+'''
+
+@Author: sheikh jainab
+
+@Date: 2024-05-17 
+
+@Last Modified by: sheikh jainab
+
+@Last Modified time: 2024-05-17 
+
+@Title : 2. Write a Python program to iteration over sets.
+
+'''
+def main():
+    my_Set ={1,2,3,4,5}
+    for ele in my_Set:
+        print(ele)
+    
+    
+if __name__ == "__main__":
+    main()
