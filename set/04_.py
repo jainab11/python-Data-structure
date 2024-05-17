5926a790c21467feca8d11baa749686f531a6bd1
jainab11 <shaikhjainab11@gmail.com>
2024-05-17 15:49:33 +0530

    [jainab] Add: python script to add element in script

---
+++ set/04_.py
@@ -0,0 +1,13 @@
+'''
+@Author: sheikh jainab
+
+@Date: 2024-05-11 
+
+@Last Modified by: sheikh jainab
+
+@Last Modified time: 2024-05-11 
+
+@Title : 4. Write a Python program to remove item(s) from set
+
+
+'''
