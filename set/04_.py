f6e9829e91963f48490d5496fcc6f95896d66595
jainab11 <shaikhjainab11@gmail.com>
2024-05-17 15:53:43 +0530

    [Jainb] Add: python script to remove element from set

--- set/04_.py
+++ set/04_.py
@@ -1,13 +1,22 @@
 '''
 @Author: sheikh jainab
 
-@Date: 2024-05-11 
+@Date: 2024-05-17 
 
 @Last Modified by: sheikh jainab
 
-@Last Modified time: 2024-05-11 
+@Last Modified time: 2024-05-17 
 
 @Title : 4. Write a Python program to remove item(s) from set
 
-
 '''
+def main():
+    my_Set = {1,24,5,6,3}
+    print("set before removing element is :",my_Set)
+    my_Set.remove(24)
+    print("set after removing elementi is :", my_Set)
+    
+    
+    
+if __name__ == "__main__":
+    main()
