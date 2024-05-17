f9c926a434768786642cecb2bf0813918da14694
jainab11 <shaikhjainab11@gmail.com>
2024-05-17 12:25:18 +0530

    [Jainab] Add: python scrip to create tuple

---
+++ tuple/01_create_tuple.py
@@ -0,0 +1,18 @@
+'''
+@Author: Sheikh Jainab
+@Date: 2024-17-05
+@Last Modified by: Author Name
+@Last Modified time: 2024-16-05
+@Title: 1. Write a Python program to create a tuple.
+'''
+
+def main():
+    """
+    Main function to execute the program.
+    """
+    my_tuple = (1,4,6)
+    print(type(my_tuple))
+    print(my_tuple)
+
+if __name__ == "__main__":
+    main()
