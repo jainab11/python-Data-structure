4421ebceb80e859b51992ac751d31d07f4026605
jainab11 <shaikhjainab11@gmail.com>
2024-05-17 12:46:35 +0530

    [Jainab] Add: python script to cretae tuple with multiple data type

---
+++ tuple/02_tuple_data_type.py
@@ -0,0 +1,18 @@
+'''
+@Author: Sheikh Jainab
+@Date: 2024-17-05
+@Last Modified by: Author Name
+@Last Modified time: 2024-16-05
+@Title: 2. Write a Python program to create a tuple with different data types..
+'''
+
+def main():
+    """
+    Main function to execute the program.
+    """
+    my_tuple = (1,4,6 ,'jainab',True, 3.4)
+    print(type(my_tuple))
+    print(my_tuple)
+
+if __name__ == "__main__":
+    main()
