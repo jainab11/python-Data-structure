8842b260aa21ff85b6a91ee8880cc6c763090738
jainab11 <shaikhjainab11@gmail.com>
2024-05-23 13:52:19 +0530

    [Jainba] Add: python script yo add member in set

---
+++ set/03_.py
@@ -0,0 +1,30 @@
+'''
+
+@Author: sheikh jainab
+
+@Date: 2024-05-11 
+
+@Last Modified by: sheikh jainab
+
+@Last Modified time: 2024-05-11 
+
+@Title : 3. Write a Python program to add member(s) in a set.
+
+
+'''
+def main():
+    """
+
+Description:
+
+Return: This function return new set 
+to add element in set we add keyword
+
+"""
+    my_set ={1,2,3,4,5}
+    my_set.add('b')
+    print(my_set)
+    
+    
+if __name__ == "__main__":
+    main()
