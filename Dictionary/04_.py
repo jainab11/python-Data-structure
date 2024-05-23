8e4abb17fcc519ee984b2ee0f962a5ae9d064e06
jainab11 <shaikhjainab11@gmail.com>
2024-05-23 13:38:58 +0530

    [Jainab] Add: python script to itrate over dict

---
+++ Dictionary/04_.py
@@ -0,0 +1,22 @@
+'''
+
+@Author: Jainab sheikh
+
+@Date: 2024-05-23
+
+@Last Modified by: Jainab sheikh
+
+@Last Modified time: 2024-05-23
+
+@Title :4. Write a Python program to iterate over dictionaries using for loops.
+
+'''
+def main():
+    my_dict={1:10, 2:20,3:30}
+    for item in my_dict:
+        print(f"{item} : {my_dict[item]}")
+   
+      
+            
+if __name__ == "__main__":
+    main()
